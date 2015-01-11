1493ff1d1cb4ecbaff7e30e8b567f4bd81666f3f
Elliot Agro <elliotagro83@gmail.com>
2015-01-11 17:46:26 +0000

    added qwest and dist files

---
+++ gruntfile.js
@@ -0,0 +1,31 @@
+'use strict';
+
+module.exports = function(grunt) {
+
+	// Unified Watch Object
+	var watchFiles = {
+		slabsLib:['src/slabs.io.js', 'lib/qwest/qwest.min.js']
+	};
+
+	// Project Configuration
+	grunt.initConfig({
+		pkg: grunt.file.readJSON('package.json'),
+		uglify: {
+			production: {
+				options: {
+					mangle: false
+				},
+				files: {
+					'dist/slabs.io.js': watchFiles.slabsLib
+				}
+			}
+		}
+	});
+
+	// Load NPM tasks
+	require('load-grunt-tasks')(grunt);
+
+	// Default task(s).
+	grunt.registerTask('default', ['uglify']);
+
+};
